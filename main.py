1e0cb22e5a17ed577d28dcb3b09b3c2eaa66caf9
Immorttalis <suparibari@gmail.com>
2025-08-20 14:20:05 +0300

    added word counter

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def get_book_text(filepath):
         # f is a file object
         file_contents = f.read()
         return file_contents
+
+def get_word_count(texts):
+    to_count = texts.split()
+    word_count = 0
+    for text in to_count:
+        word_count += 1
+    return word_count
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    #print(get_book_text("books/frankenstein.txt"))
+    print(f"{get_word_count(get_book_text("books/frankenstein.txt"))} words found in the document")
     
 
 main()
